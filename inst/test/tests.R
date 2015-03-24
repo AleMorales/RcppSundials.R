e8efa9f197694d358adba2383ccd19fa9d1918d9
AleMorales <morales.s.alejandro@gmail.com>
2015-03-24 09:17:52 +0100

    Update tests to the latest version of the package.

--- inst/test/tests.R
+++ inst/test/tests.R
@@ -1,28 +1,30 @@
-library(RcppSundials)
-library(microbenchmark)
-
-example_model_R = function(t, states, parameters, forcings) {
-   derivatives = -states*parameters[1];
-   observed = forcings[1];
-   return(list(derivatives, observed));
-}
-
-example_jacobian_R = function(t, states, parameters, forcings) {
-   output = matrix(0, length(states), length(states));
-   diag(output) = -parameters[1];
-   return(output);
-}
-
-# Use the Cpp version that is wrapped in R
-simulationR = cvode_R(times = 1:5, 
-                     states = rep(1,4), 
-                     parameters = 0.1, 
-                     forcings_data = list(cbind(1:3600,1:3600)),
-                     settings = list(rtol = 1e-6, 
-                      atol = 1e-6, maxsteps = 1e3, maxord = 5, hini = 1e-3, 
-                      hmin = 0, hmax = 100, maxerr = 5, maxnonlin = 10,
-                      maxconvfail = 10, method = "bdf", maxtime = 1, jacobian = 0), 
-                     model = example_model_R, jacobian = example_jacobian_R)
+  library(RcppSundials)
+  library(microbenchmark)
+  
+  example_model_R = function(t, states, parameters, forcings) {
+     derivatives = -states*parameters[1];
+     observed = forcings[1];
+     return(list(derivatives, observed));
+  }
+  
+  example_jacobian_R = function(t, states, parameters, forcings) {
+     output = matrix(0, length(states), length(states));
+     diag(output) = -parameters[1];
+     return(output);
+  }
+  
+  # Use the Cpp version that is wrapped in R
+  simulationR = cvode_R(times = 1:1e3, 
+                       states = rep(1,5), 
+                       parameters = 0.1, 
+                       forcings_data = list(cbind(1:3600,1:3600)),
+                       settings = list(rtol = 1e-6, 
+                        atol = 1e-6, maxsteps = 1e5, maxord = 5, hini = 0, 
+                        hmin = 0, hmax = 0, maxerr = 5, maxnonlin = 10,
+                        maxconvfail = 10, method = "bdf", jacobian = 0,
+                        minimum = -1e-4, positive = 1,
+                        which_states = 1, which_observed = integer()), 
+                       model = example_model_R, jacobian = example_jacobian_R)
 
 stl_example_model = getNativeSymbolInfo(name = "example_model_stl",
                                              PACKAGE = "RcppSundials")$address
@@ -30,17 +32,28 @@ stl_example_jacobian = getNativeSymbolInfo(name = "example_jacobian_stl",
                                              PACKAGE = "RcppSundials")$address
 
 simulationstl = cvode_Cpp_stl(times = 1:5, 
-                     states = rep(1.0,4), 
+                     states = rep(1.0,5), 
                      parameters = 0.1, 
                      forcings_data =list(cbind(1:3600,1:3600)),
                      settings = list(rtol = 1e-6,
                       atol = 1e-6, maxsteps = 1e3, maxord = 5, hini = 0,
                       hmin = 0, hmax = 100, maxerr = 5, maxnonlin = 10,
-                      maxconvfail = 10, method = "bdf", maxtime = 0, jacobian = 0), 
-                      model = stl_example_model, jacobian = "wat?") 
+                      maxconvfail = 10, method = "bdf", jacobian = 0,
+                      minimum = -1e-4, positive = 1,
+                      which_states = 1, which_observed = integer()), 
+                      model = stl_example_model, jacobian = stl_example_jacobian) 
+
+cvode_calc_derivs(stl_example_model, t = 0,  states = rep(1.0,5), parameters = 0.1, 
+            forcings_data =list(cbind(1:3600,1:3600)))
+cvode_calc_jac(stl_example_jacobian, t = 0,  states = rep(1.0,5), parameters = 0.1, 
+            forcings_data =list(cbind(1:3600,1:3600)))
 
 stl_example_dae = getNativeSymbolInfo(name = "example_dae_stl",
                                              PACKAGE = "RcppSundials")$address
+
+ida_calc_derivs(stl_example_dae, t = 0,  states = c(1,0,0), derivatives = c(-0.04,0.04,0),
+                parameters = 0.1, forcings_data =list(cbind(1:3600,1:3600)))
+
 
 simulationstl = ida_Cpp_stl(times = c(0,0.4,4,40,400), 
                      states = c(1,0,0), 
@@ -50,27 +63,58 @@ simulationstl = ida_Cpp_stl(times = c(0,0.4,4,40,400),
                      settings = list(rtol = 1e-10, 
                       atol = 1e-10, maxsteps = 1e4, maxord = 5, hini = 0, 
                       hmin = 0, hmax = 0, maxerr = 5, maxnonlin = 10,
-                      maxconvfail = 10, method = "bdf", maxtime = 0, jacobian = 0), 
+                      maxconvfail = 10, method = "bdf", jacobian = 0,
+                      minimum = -1e-4, positive = 1, which_states = 1:3, which_observed = 1), 
                       model = stl_example_dae, jacobian = "wat?") 
 
 
-microbenchmark::microbenchmark(cvode_R(times = 1:5, 
-                     states = rep(1,4), 
+for(i in 1:10) {
+  test = cvode_R(times = 1:1e3, 
+                     states = rep(1,1e3), 
                      parameters = 0.1, 
                      forcings_data = list(cbind(1:3600,1:3600)),
                      settings = list(rtol = 1e-6, 
-                      atol = 1e-6, maxsteps = 1e3, maxord = 5, hini = 1e-3, 
-                      hmin = 0, hmax = 100, maxerr = 5, maxnonlin = 10,
-                      maxconvfail = 10, method = "bdf", maxtime = 1, jacobian = 0), 
-                     model = example_model_R, jacobian = example_jacobian_R))
+                      atol = 1e-6, maxsteps = 1e4, maxord = 5, hini = 0, 
+                      hmin = 0, hmax = 0, maxerr = 5, maxnonlin = 10,
+                      maxconvfail = 10, method = "bdf", jacobian = 0,
+                      minimum = -1e-4, positive = 1,which_states = 1:5,which_observed = 1), 
+                     model = example_model_R, jacobian = example_jacobian_R)
+}
+
+
+for(i in 1:100) {
+  print(i)
+  test = cvode_Cpp_stl(times = 1:1e3, 
+                     states = rep(1.0,1e2), 
+                     parameters = 0.0001, 
+                     forcings_data =list(cbind(1:3600,1:3600)),
+                     settings = list(rtol = 1e-6, 
+                      atol = 1e-6, maxsteps = 1e4, maxord = 5, hini = 0, 
+                      hmin = 0, hmax = 0, maxerr = 5, maxnonlin = 10,
+                      maxconvfail = 10, method = "bdf", jacobian = 0,
+                      minimum = -1e-4, positive = 0,which_states = 1:5,which_observed = integer()), 
+                      model = stl_example_model, jacobian = stl_example_jacobian)
+}
+
+microbenchmark::microbenchmark(test = cvode_R(times = 1:1e3, 
+                     states = rep(1,1e3), 
+                     parameters = 0.1, 
+                     forcings_data = list(cbind(1:3600,1:3600)),
+                     settings = list(rtol = 1e-6, 
+                      atol = 1e-6, maxsteps = 1e4, maxord = 5, hini = 0, 
+                      hmin = 0, hmax = 0, maxerr = 5, maxnonlin = 10,
+                      maxconvfail = 10, method = "bdf", jacobian = 0,
+                      minimum = -1e-4, positive = 1,which_states = 1:5,which_observed = 1), 
+                     model = example_model_R, jacobian = example_jacobian_R), times = 4)
 
              
-microbenchmark::microbenchmark(cvode_Cpp_stl(times = 1:5, 
-                     states = rep(1.0,4), 
-                     parameters = 0.1, 
+microbenchmark::microbenchmark(test = cvode_Cpp_stl(times = 1:1e3, 
+                     states = rep(1000.0,1e2), 
+                     parameters = 0.001, 
                      forcings_data =list(cbind(1:3600,1:3600)),
-                     settings = list(rtol = 1e-6,
-                      atol = 1e-6, maxsteps = 1e3, maxord = 5, hini = 0,
-                      hmin = 0, hmax = 100, maxerr = 5, maxnonlin = 10,
-                      maxconvfail = 10, method = "bdf", maxtime = 0, jacobian = 0), 
-                      model = stl_example_model, jacobian = stl_example_jacobian) )
+                     settings = list(rtol = 1e-6, 
+                      atol = 1e-6, maxsteps = 1e4, maxord = 5, hini = 0, 
+                      hmin = 0, hmax = 0, maxerr = 5, maxnonlin = 10,
+                      maxconvfail = 10, method = "bdf", jacobian = 1,
+                      minimum = -1e-4, positive = 0,which_states = 1:1e2,which_observed = integer(1)), 
+                      model = stl_example_model, jacobian = stl_example_jacobian), times = 10)
