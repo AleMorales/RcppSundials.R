fdbf9f83ae4f029eb8a58a302972fcd1bf57a3e9
AleMorales <morales.s.alejandro@gmail.com>
2015-03-24 09:21:59 +0100

    Update NAMESPACE and test file to check for segfaults. The R version may segfault when running multiple simulations and large number of state variables. Not clear where the problem is coming from.

--- inst/test/tests.R
+++ inst/test/tests.R
@@ -45,13 +45,13 @@ simulationstl = cvode_Cpp_stl(times = 1:5,
 
 cvode_calc_derivs(stl_example_model, t = 0,  states = rep(1.0,5), parameters = 0.1, 
             forcings_data =list(cbind(1:3600,1:3600)))
-cvode_calc_jac(stl_example_jacobian, t = 0,  states = rep(1.0,5), parameters = 0.1, 
+jac = cvode_calc_jac(stl_example_jacobian, t = 0,  states = rep(1.0,5), parameters = 0.1, 
             forcings_data =list(cbind(1:3600,1:3600)))
 
 stl_example_dae = getNativeSymbolInfo(name = "example_dae_stl",
                                              PACKAGE = "RcppSundials")$address
 
-ida_calc_derivs(stl_example_dae, t = 0,  states = c(1,0,0), derivatives = c(-0.04,0.04,0),
+ida_calc_res(stl_example_dae, t = 0,  states = c(1,0,0), derivatives = c(-0.04,0.04,0),
                 parameters = 0.1, forcings_data =list(cbind(1:3600,1:3600)))
 
 
@@ -70,7 +70,7 @@ simulationstl = ida_Cpp_stl(times = c(0,0.4,4,40,400),
 
 for(i in 1:10) {
   test = cvode_R(times = 1:1e3, 
-                     states = rep(1,1e3), 
+                     states = rep(1000,1e2), 
                      parameters = 0.1, 
                      forcings_data = list(cbind(1:3600,1:3600)),
                      settings = list(rtol = 1e-6, 
@@ -81,9 +81,8 @@ for(i in 1:10) {
                      model = example_model_R, jacobian = example_jacobian_R)
 }
 
-
+# Check for segfaults associated to memory leaks
 for(i in 1:100) {
-  print(i)
   test = cvode_Cpp_stl(times = 1:1e3, 
                      states = rep(1.0,1e2), 
                      parameters = 0.0001, 
@@ -94,6 +93,25 @@ for(i in 1:100) {
                       maxconvfail = 10, method = "bdf", jacobian = 0,
                       minimum = -1e-4, positive = 0,which_states = 1:5,which_observed = integer()), 
                       model = stl_example_model, jacobian = stl_example_jacobian)
+  cvode_calc_derivs(stl_example_model, t = 0,  states = rep(1.0,5), parameters = 0.1, 
+            forcings_data =list(cbind(1:3600,1:3600)))
+  jac = cvode_calc_jac(stl_example_jacobian, t = 0,  states = rep(1.0,5), parameters = 0.1, 
+            forcings_data =list(cbind(1:3600,1:3600)))
+  
+ida_calc_res(stl_example_dae, t = 0,  states = c(1,0,0), derivatives = c(-0.04,0.04,0),
+                parameters = 0.1, forcings_data =list(cbind(1:3600,1:3600)))
+
+simulationstl = ida_Cpp_stl(times = c(0,0.4,4,40,400), 
+                     states = c(1,0,0), 
+                     derivatives = c(-0.04,0.04,0),
+                     parameters = 0.1, 
+                     forcings_data =list(cbind(1:3600,1:3600)),
+                     settings = list(rtol = 1e-10, 
+                      atol = 1e-10, maxsteps = 1e4, maxord = 5, hini = 0, 
+                      hmin = 0, hmax = 0, maxerr = 5, maxnonlin = 10,
+                      maxconvfail = 10, method = "bdf", jacobian = 0,
+                      minimum = -1e-4, positive = 1, which_states = 1:3, which_observed = 1), 
+                      model = stl_example_dae, jacobian = "wat?")   
 }
 
 microbenchmark::microbenchmark(test = cvode_R(times = 1:1e3, 
