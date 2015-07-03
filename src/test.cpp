4b783416850b0dafb2b131182d8b4092cb4b3f97
Alejandro Morales Sierra <morales.s.alejandro@gmail.com>
2015-07-03 16:24:03 +0200

    Remove examples using Rcpp

--- src/test.cpp
+++ src/test.cpp
@@ -10,27 +10,6 @@ using namespace Rcpp;
  This file contains a series of tests 
  */
 
-//' An example of model (unit test)
-//' @export
-// [[Rcpp::export]]
-List example_model(double t, NumericVector states, NumericVector parameters, NumericVector forcings) {
-   NumericVector derivatives = -states*parameters[0];
-   NumericVector observed{forcings[0]};
-   return List::create(derivatives, observed);
-}
-
-//' An example of a model's Jacobian matrix (unit test)
-//' @export
-// [[Rcpp::export]]
-NumericMatrix example_jacobian(double t, NumericVector states, NumericVector parameters, NumericVector forcings) {
-   NumericMatrix output(states.size(), states.size());
-   for(int i = 0; i < states.size(); i++) {
-     output(i,i) = -parameters[0];
-   }
-   return output;
-}
-
-
 extern "C" {
   array<vector<double>, 2> example_model_stl(const double& t, const vector<double>& states, 
             const vector<double>& parameters, const vector<double>& forcings) {
