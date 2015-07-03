b1de942c42a652f2c2dd2c9e3fe0e667f14a2f0b
Alejandro Morales Sierra <morales.s.alejandro@gmail.com>
2015-07-03 16:31:16 +0200

    RcppExports is generate automatically. New version committed.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -9,42 +9,6 @@
 
 using namespace Rcpp;
 
-// cvode_R
-NumericMatrix cvode_R(NumericVector times, NumericVector states, NumericVector parameters, List forcings_data, List settings, Function model, Function jacobian);
-static SEXP RcppSundials_cvode_R_try(SEXP timesSEXP, SEXP statesSEXP, SEXP parametersSEXP, SEXP forcings_dataSEXP, SEXP settingsSEXP, SEXP modelSEXP, SEXP jacobianSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::traits::input_parameter< NumericVector >::type times(timesSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type states(statesSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type parameters(parametersSEXP);
-    Rcpp::traits::input_parameter< List >::type forcings_data(forcings_dataSEXP);
-    Rcpp::traits::input_parameter< List >::type settings(settingsSEXP);
-    Rcpp::traits::input_parameter< Function >::type model(modelSEXP);
-    Rcpp::traits::input_parameter< Function >::type jacobian(jacobianSEXP);
-    __result = Rcpp::wrap(cvode_R(times, states, parameters, forcings_data, settings, model, jacobian));
-    return __result;
-END_RCPP_RETURN_ERROR
-}
-RcppExport SEXP RcppSundials_cvode_R(SEXP timesSEXP, SEXP statesSEXP, SEXP parametersSEXP, SEXP forcings_dataSEXP, SEXP settingsSEXP, SEXP modelSEXP, SEXP jacobianSEXP) {
-    SEXP __result;
-    {
-        Rcpp::RNGScope __rngScope;
-        __result = PROTECT(RcppSundials_cvode_R_try(timesSEXP, statesSEXP, parametersSEXP, forcings_dataSEXP, settingsSEXP, modelSEXP, jacobianSEXP));
-    }
-    Rboolean __isInterrupt = Rf_inherits(__result, "interrupted-error");
-    if (__isInterrupt) {
-        UNPROTECT(1);
-        Rf_onintr();
-    }
-    Rboolean __isError = Rf_inherits(__result, "try-error");
-    if (__isError) {
-        SEXP __msgSEXP = Rf_asChar(__result);
-        UNPROTECT(1);
-        Rf_error(CHAR(__msgSEXP));
-    }
-    UNPROTECT(1);
-    return __result;
-}
 // cvode_Cpp_stl
 NumericMatrix cvode_Cpp_stl(NumericVector times, NumericVector states_, NumericVector parameters_, List forcings_data_, List settings, SEXP model_, SEXP jacobian_);
 static SEXP RcppSundials_cvode_Cpp_stl_try(SEXP timesSEXP, SEXP states_SEXP, SEXP parameters_SEXP, SEXP forcings_data_SEXP, SEXP settingsSEXP, SEXP model_SEXP, SEXP jacobian_SEXP) {
@@ -149,6 +113,42 @@ RcppExport SEXP RcppSundials_cvode_calc_jac(SEXP jacobian_SEXP, SEXP tSEXP, SEXP
     UNPROTECT(1);
     return __result;
 }
+// cvode_R
+NumericMatrix cvode_R(NumericVector times, NumericVector states, NumericVector parameters, List forcings_data, List settings, Function model, Function jacobian);
+static SEXP RcppSundials_cvode_R_try(SEXP timesSEXP, SEXP statesSEXP, SEXP parametersSEXP, SEXP forcings_dataSEXP, SEXP settingsSEXP, SEXP modelSEXP, SEXP jacobianSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::traits::input_parameter< NumericVector >::type times(timesSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type states(statesSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type parameters(parametersSEXP);
+    Rcpp::traits::input_parameter< List >::type forcings_data(forcings_dataSEXP);
+    Rcpp::traits::input_parameter< List >::type settings(settingsSEXP);
+    Rcpp::traits::input_parameter< Function >::type model(modelSEXP);
+    Rcpp::traits::input_parameter< Function >::type jacobian(jacobianSEXP);
+    __result = Rcpp::wrap(cvode_R(times, states, parameters, forcings_data, settings, model, jacobian));
+    return __result;
+END_RCPP_RETURN_ERROR
+}
+RcppExport SEXP RcppSundials_cvode_R(SEXP timesSEXP, SEXP statesSEXP, SEXP parametersSEXP, SEXP forcings_dataSEXP, SEXP settingsSEXP, SEXP modelSEXP, SEXP jacobianSEXP) {
+    SEXP __result;
+    {
+        Rcpp::RNGScope __rngScope;
+        __result = PROTECT(RcppSundials_cvode_R_try(timesSEXP, statesSEXP, parametersSEXP, forcings_dataSEXP, settingsSEXP, modelSEXP, jacobianSEXP));
+    }
+    Rboolean __isInterrupt = Rf_inherits(__result, "interrupted-error");
+    if (__isInterrupt) {
+        UNPROTECT(1);
+        Rf_onintr();
+    }
+    Rboolean __isError = Rf_inherits(__result, "try-error");
+    if (__isError) {
+        SEXP __msgSEXP = Rf_asChar(__result);
+        UNPROTECT(1);
+        Rf_error(CHAR(__msgSEXP));
+    }
+    UNPROTECT(1);
+    return __result;
+}
 // ida_Cpp_stl
 NumericMatrix ida_Cpp_stl(NumericVector times, NumericVector states_, NumericVector derivatives_, NumericVector parameters_, List forcings_data_, List settings, SEXP model_, SEXP jacobian_);
 RcppExport SEXP RcppSundials_ida_Cpp_stl(SEXP timesSEXP, SEXP states_SEXP, SEXP derivatives_SEXP, SEXP parameters_SEXP, SEXP forcings_data_SEXP, SEXP settingsSEXP, SEXP model_SEXP, SEXP jacobian_SEXP) {
@@ -198,53 +198,25 @@ BEGIN_RCPP
     return __result;
 END_RCPP
 }
-// example_model
-List example_model(double t, NumericVector states, NumericVector parameters, NumericVector forcings);
-RcppExport SEXP RcppSundials_example_model(SEXP tSEXP, SEXP statesSEXP, SEXP parametersSEXP, SEXP forcingsSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< double >::type t(tSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type states(statesSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type parameters(parametersSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type forcings(forcingsSEXP);
-    __result = Rcpp::wrap(example_model(t, states, parameters, forcings));
-    return __result;
-END_RCPP
-}
-// example_jacobian
-NumericMatrix example_jacobian(double t, NumericVector states, NumericVector parameters, NumericVector forcings);
-RcppExport SEXP RcppSundials_example_jacobian(SEXP tSEXP, SEXP statesSEXP, SEXP parametersSEXP, SEXP forcingsSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< double >::type t(tSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type states(statesSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type parameters(parametersSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type forcings(forcingsSEXP);
-    __result = Rcpp::wrap(example_jacobian(t, states, parameters, forcings));
-    return __result;
-END_RCPP
-}
 
 // validate (ensure exported C++ functions exist before calling them)
 static int RcppSundials_RcppExport_validate(const char* sig) { 
     static std::set<std::string> signatures;
     if (signatures.empty()) {
-        signatures.insert("NumericMatrix(*cvode_R)(NumericVector,NumericVector,NumericVector,List,List,Function,Function)");
         signatures.insert("NumericMatrix(*cvode_Cpp_stl)(NumericVector,NumericVector,NumericVector,List,List,SEXP,SEXP)");
         signatures.insert("List(*cvode_calc_derivs)(SEXP,NumericVector,NumericVector,NumericVector,List)");
         signatures.insert("NumericMatrix(*cvode_calc_jac)(SEXP,NumericVector,NumericVector,NumericVector,List)");
+        signatures.insert("NumericMatrix(*cvode_R)(NumericVector,NumericVector,NumericVector,List,List,Function,Function)");
     }
     return signatures.find(sig) != signatures.end();
 }
 
 // registerCCallable (register entry points for exported C++ functions)
 RcppExport SEXP RcppSundials_RcppExport_registerCCallable() { 
-    R_RegisterCCallable("RcppSundials", "RcppSundials_cvode_R", (DL_FUNC)RcppSundials_cvode_R_try);
     R_RegisterCCallable("RcppSundials", "RcppSundials_cvode_Cpp_stl", (DL_FUNC)RcppSundials_cvode_Cpp_stl_try);
     R_RegisterCCallable("RcppSundials", "RcppSundials_cvode_calc_derivs", (DL_FUNC)RcppSundials_cvode_calc_derivs_try);
     R_RegisterCCallable("RcppSundials", "RcppSundials_cvode_calc_jac", (DL_FUNC)RcppSundials_cvode_calc_jac_try);
+    R_RegisterCCallable("RcppSundials", "RcppSundials_cvode_R", (DL_FUNC)RcppSundials_cvode_R_try);
     R_RegisterCCallable("RcppSundials", "RcppSundials_RcppExport_validate", (DL_FUNC)RcppSundials_RcppExport_validate);
     return R_NilValue;
 }
